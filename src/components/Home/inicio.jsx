a4c91727dc2abd3ced19b941f06de5605324289b
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-20 22:00:32 -0500

    00

--- src/components/Home/inicio.jsx
+++ src/components/Home/inicio.jsx
@@ -5,9 +5,6 @@ import IMG1 from '../../../public/1373004photo-1590656223930-04c6d0eeb13bjpg.jpg
 function inicio() {
   return (
     <div className='inicio'>
-
-
-        
         <div className='texbut'>
             <h1>Aplicaciones portátiles increíbles</h1>
             <div className='buttons'>
@@ -20,7 +17,7 @@ function inicio() {
             </div>
         </div>
         <div className='Img'>
-            <img className={{IMG1}} style={with'300px'} src="../../../public/1373004photo-1590656223930-04c6d0eeb13bjpg.jpg" alt="imagen" />
+            <img className={{IMG1}} src="../../../public/1373004photo-1590656223930-04c6d0eeb13bjpg.jpg" alt="imagen" />
         </div>
     </div>
   )
