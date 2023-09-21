9850a2e894307ff9f04a141cca6cfe05195b0d8f
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-20 20:21:43 -0500

    8:21

---
+++ src/components/Home/inicio.jsx
@@ -0,0 +1,21 @@
+
+function inicio() {
+  return (
+    <div className='inicio'>
+        <div className='texbut'>
+            <h1>Aplicaciones portátiles increíbles</h1>
+            <div className='buttons'>
+                <button className='button'>
+                    <p>Explorar ahora</p>
+                </button>
+                <button className='button'>
+                    <p>Comenzar comprar</p>
+                </button>
+            </div>
+        </div>
+        <div className='grid'></div>
+    </div>
+  )
+}
+
+export default inicio
