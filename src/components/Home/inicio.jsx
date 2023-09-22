131d150872b61d43426a407fec7e3297bf18137f
edinsonRmz <edinsonramirez0627@gmail.com>
2023-09-21 22:41:47 -0500

    1041

--- src/components/Home/inicio.jsx
+++ src/components/Home/inicio.jsx
@@ -1,27 +1,25 @@
 import Usb from '../../Img/istockphoto-1404865576-612x612.jpg';
 import Bolsa from '../../Img/png-clipart-e-commerce-webstore-affiliate-marketing-business-shopping-bag-web-design-rectangle.png';
-
+import inicios from './inicio.css';
 
 function inicio() {
   return (
-    <div className='inicio' style={{ }}>
-        <div className='texbut' 
-            style={{ width:'1px', height:'auto', justifyContent:'center',
-            alignItems:'flex-start', position:'relative',  }}>
-            <h1 className='texto' style={{ }}>Aplicaciones portátiles increíbles</h1>
+    <div className='inicio'>
+        <div className='texbut'>
+            <h1 className='texto'>Aplicaciones portátiles increíbles</h1>
 
             <div className='buttons'>
                 <button className='button'>
                     <p>Explorar ahora</p>
                 </button>
                 <button className='button'>
-                    <p>Comenzar comprar</p>
+                    <p className='comenzar-compra'>Comenzar comprar</p>
                 </button>
             </div>
         </div>
             <div className='imageness'>
-                 <img className='Usb' style={{width:'300px', height:'300px'}} src={Usb} alt="ImagenUSB" />
-                 <img className='bolsa' style={{width:'300px', height:'400px'}}  src={Bolsa}  alt="Bolsa" />
+                 <img className='Usb' src={Usb} alt="ImagenUSB" />
+                 <img className='bolsa'  src={Bolsa}  alt="Bolsa" />
             </div>
            
         
